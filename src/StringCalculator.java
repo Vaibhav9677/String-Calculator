2f82e777d705d36019a63bde9fddce0a7bbc82c8
Vaibhav Pawar <vaibhavhpawar5@gmail.com>
2025-09-17 21:51:14 +0530

    Commit after passing test2 : String with number 1 and 2

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,10 +2,19 @@
 public class StringCalculator {
 
     public int add(String numbers) {
-        if (numbers.isEmpty()) {
+        
+        //numbers (string) is empty or null
+        if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
 
-        return 1;
+        String[] parts = numbers.split(",");
+        int sum = 0;
+
+        for (String part : parts) {
+            sum += Integer.parseInt(part.trim());
+        }
+
+        return sum;
     }
 }
