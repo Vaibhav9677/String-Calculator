c4ee576e680b04957f553c0d4b9e81f2e4936061
Vaibhav Pawar <vaibhavhpawar5@gmail.com>
2025-09-17 22:13:22 +0530

    Commit after passing test3 : Numbers separated by newlines should be summed

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,16 +1,21 @@
 
 public class StringCalculator {
 
-    public int add(String numbers) {
-        
+    int sum;
+
+    public StringCalculator()
+    {
+        this.sum = 0;
+    }
+
+
+    public int add(String numbers) {   
         //numbers (string) is empty or null
         if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
-
         // Split by comma OR newline
         String[] parts = numbers.split("[,\n]");
-        int sum = 0;
 
         for (String part : parts) {
             if (!part.trim().isEmpty()) {
